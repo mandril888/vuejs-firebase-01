9b60a10fd23b499a4b95444570e5df2059f84662
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2017-05-28 13:32:04 +0200

    implement add and remove lawyers

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,20 +1,45 @@
 <template>
   <div class="hello">
     <div class="panel-body">
+      <h2>{{ msg }}</h2>
       <table class="table table-striped">
         <thead>
           <tr>
             <th>Abogado</th>
             <th>Email</th>
+            <th>Remove</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="abogado in abogados">
             <td><a v-bind:href="abogado.url">{{abogado.name}}</a></td>
             <td>{{abogado.email}}</td>
+            <td><span class="glyphicon glyphicon-trash" aria-hidden="true" v-on:click="removeLawyer(lawyer)"></span></td>
           </tr>
         </tbody>
       </table>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Add New Lawyer</h3>
+      </div>
+      <div class="panel-body">
+         <form id="form" class="form-inline" v-on:submit.prevent="addLawyer">
+          <div class="form-group">
+            <label for="lawyerName">Name:</label>
+            <input type="text" id="lawyerName" class="form-control" v-model="newLawyer.name">
+          </div>
+          <div class="form-group">
+            <label for="layerEmail">Email:</label>
+            <input type="text" id="layerEmail" class="form-control" v-model="newLawyer.email">
+          </div>
+          <div class="form-group">
+            <label for="lawyerUrl">Url:</label>
+            <input type="text" id="lawyerUrl" class="form-control" v-model="newLawyer.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Lawyer">
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -40,11 +65,27 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to my Vue.js App',
+      newLawyer: {
+        name: '',
+        email: '',
+        url: 'http://'
+      }
     }
   },
   firebase: {
     abogados: abogadosRef
+  },
+  methods: {
+    addLawyer: function () {
+      abogadosRef.push(this.newLawyer)
+      this.newLawyer.name = ''
+      this.newLawyer.gmail = ''
+      this.newLawyer.url = 'http://'
+    },
+    removeLawyer: function (lawyer) {
+      abogadosRef.child(lawyer['.key']).remove()
+    }
   }
 }
 </script>
