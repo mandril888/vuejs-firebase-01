c63e14eabee07b9a8b534d0ac5f6a98f84786517
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2017-05-28 13:15:51 +0200

    BBDD conected to Hello route view

--- src/App.vue
+++ src/App.vue
@@ -7,32 +7,8 @@
 
 <script src="https://www.gstatic.com/firebasejs/4.0.0/firebase.js"></script>
 <script>
-  import Vue from 'vue'
-  import Firebase from 'firebase'
-
-  // Initialize Firebase
-  let config = {
-    apiKey: 'AIzaSyA8_JgoOz1mr8o9T6Wvhlf9YCh4BT6raBw',
-    authDomain: 'infante-abogados.firebaseapp.com',
-    databaseURL: 'https://infante-abogados.firebaseio.com',
-    projectId: 'infante-abogados',
-    storageBucket: 'infante-abogados.appspot.com',
-    messagingSenderId: '796651083135'
-  }
-
-  let app = Firebase.initializeApp(config)
-  let db = app.database()
-  let abogadosRef = db.ref('abogados')
-
-  Vue.component('firebase', {
-    abogados: abogadosRef
-  })
-
   export default {
-    name: 'app',
-    firebase: {
-      abogados: abogadosRef
-    }
+    name: 'app'
   }
 </script>
 
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -16,33 +16,35 @@
         </tbody>
       </table>
     </div>
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
+
+// Initialize Firebase
+let config = {
+  apiKey: 'AIzaSyA8_JgoOz1mr8o9T6Wvhlf9YCh4BT6raBw',
+  authDomain: 'infante-abogados.firebaseapp.com',
+  databaseURL: 'https://infante-abogados.firebaseio.com',
+  projectId: 'infante-abogados',
+  storageBucket: 'infante-abogados.appspot.com',
+  messagingSenderId: '796651083135'
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let abogadosRef = db.ref('abogados')
+
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  firebase: {
+    abogados: abogadosRef
   }
 }
 </script>
